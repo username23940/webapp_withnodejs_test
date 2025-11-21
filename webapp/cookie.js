66deb09a36c8540d0273889e04cb4ccac2a03980
LIM JANGBIN <great_jb2005@naver.com>
2025-11-21 23:26:32 +0900

    Update cookie.js

--- webapp/cookie.js
+++ webapp/cookie.js
@@ -13,8 +13,8 @@ http.createServer(function(request, response){
                      `Permanent=cookies; Max-Age=${60*60*24*30}`], // 30일
                       'Secure=Secure; Secure' // key = value; Secure
                       'HttpOnly=HttpOnly; HttpOnly' // 마찬가지
-                    'Path=Path; Path=/cookie',
-                    'Doamin=Domain; Domain=test.o2.org'
+                    'Path=Path; Path=/cookie', //  /cookie에만 살아있는 쿠키
+                    'Doamin=Domain; Domain=o2.org' // o2.org에 어떤 서브 도메인이 와도 살아남음
     });
     response.end('Cookie!!');
 }).listen(3000);
